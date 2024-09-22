ebcdb1017243b534af489b44c2ca8465acd3d0d6
Dean Kinyua <kinyuadean@gmail.com>
2024-09-22 16:18:15 +0300

    use reg.test to test if a value matches a regular expression

--- validation.js
+++ validation.js
@@ -1,1 +1,22 @@
 // validation script here
+const inputs = document.querySelectorAll("input");
+
+const patterns = {
+  username: /^[a-zA-Z]{5,12}$/gi,
+  telephone: /^0(7|1)\d{8}$/gi,
+};
+
+const validate = (field, regex) => {
+  if (regex.test(field.value)) {
+    field.className = "valid";
+  } else {
+    field.className = "invalid";
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", (e) => {
+    // console.log(e.target.value);
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
