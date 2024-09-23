32fe8d96e50cddc9248f591383971b1c59f1d342
Dean Kinyua <kinyuadean@gmail.com>
2024-09-23 14:42:30 +0300

    the global flag was bringing some mess that i am about to investigate

--- validation.js
+++ validation.js
@@ -2,15 +2,21 @@
 const inputs = document.querySelectorAll("input");
 
 const patterns = {
-  username: /^[a-zA-Z]{5,12}$/gi,
-  telephone: /^0(7|1)\d{8}$/gi,
+  username: /^[a-z\d]{5,12}$/i,
+  telephone: /^0(7|1)\d{8}$/i,
+  password: /^[\w@-]{8,20}$/i,
+  slug: /^[a-z\d-]{8,20}$/,
 };
 
 const validate = (field, regex) => {
-  if (regex.test(field.value)) {
-    field.className = "valid";
+  if (regex.test(field.value) === true) {
+    field.classList.remove("invalid");
+
+    field.classList.add("valid");
   } else {
-    field.className = "invalid";
+    field.classList.remove("valid");
+
+    field.classList.add("invalid");
   }
 };
 
@@ -20,3 +26,15 @@ inputs.forEach((input) => {
     validate(e.target, patterns[e.target.attributes.name.value]);
   });
 });
+
+// sticky property in regex
+// const str = "#foo#";
+// const regex = /foo/y;
+
+// regex.lastIndex = 1;
+// reg1 = regex.test(str); // true
+// console.log(`reg 1 ${reg1}`);
+// regex.lastIndex = 5;
+// reg2 = regex.test(str); // false (lastIndex is taken into account with sticky flag)
+// console.log(`reg 2 ${reg2}`);
+// regex.lastIndex; // 0 (reset after match failure)
